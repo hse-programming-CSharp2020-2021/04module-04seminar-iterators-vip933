acda7f3e086e0d928f9dbbcedf7a7c5e51f184a5
vip933 <71464354+vip933@users.noreply.github.com>
2021-05-07 12:08:25 +0300

    Update Program.cs

--- Iterators/Task05/Program.cs
+++ Iterators/Task05/Program.cs
@@ -1,5 +1,6 @@
-﻿using System;
+using System;
 using System.Collections;
+using System.Text;
 
 /* На вход подается число N.
  * Нужно создать коллекцию из N элементов последовательного ряда натуральных чисел, возведенных в 10 степень, 
@@ -30,6 +31,9 @@ namespace Task05
         {
             try
             {
+                if (!int.TryParse(Console.ReadLine(), out int value) || value < 1)
+                    throw new ArgumentException();
+
                 MyDigits myDigits = new MyDigits();
                 IEnumerator enumerator = myDigits.MyEnumerator(value);
 
@@ -51,15 +55,77 @@ namespace Task05
 
         static void IterateThroughEnumeratorWithoutUsingForeach(IEnumerator enumerator)
         {
+            StringBuilder sb = new StringBuilder();
+            while (enumerator.MoveNext())
+            {
+                sb.Append($"{enumerator.Current} ");
+            }
+            Console.Write(sb.Remove(sb.Length - 1, 1));
         }
     }
 
     class MyDigits : IEnumerator // НЕ МЕНЯТЬ ЭТУ СТРОКУ
     {
+        bool IsForward = true;
+        int stop;
+        int position = 0;
+
+        public IEnumerator MyEnumerator(int value)
+        {
+            this.stop = value;
+            return GetEnumerator();
+        }
+
+        public MyDigits GetEnumerator()
+        {
+            return this;
+        }
 
         public bool MoveNext()
         {
-           
+            if (IsForward)
+            {
+                if (position < stop)
+                {
+                    position++;
+                    return true;
+                }
+                else
+                {
+                    IsForward = false;
+                    position++;
+                    return false;
+                }
+            }
+            else
+            {
+                if (position > 1)
+                {
+                    position--;
+                    return true;
+                }
+                else
+                {
+                    IsForward = true;
+                    position--;
+                    return false;
+                }
+            }
+        }
+
+        public object Current
+        {
+            get
+            {
+                if (position == 0 || position > stop)
+                    throw new ArgumentException();
+                return Math.Pow(position, 10);
+            }
+        }
+
+        public void Reset()
+        {
+            position = 0;
         }
 
     }
